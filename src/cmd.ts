cee9dd9cb08d764b787d9292d8e1838da36095f2
ysmtegsr <38056766+ysmtegsr@users.noreply.github.com>
2021-04-17 16:35:36 +0900

    feat: slack notification

--- src/cmd.ts
+++ src/cmd.ts
@@ -1,18 +1,38 @@
 import * as yargs from 'yargs'
 
+function getPercent(): number {
+  const now = new Date()
+  const startOfYear = new Date(`${now.getFullYear()}`).getTime()
+  const endOfYear = new Date(`${now.getFullYear() + 1}`).getTime()
+
+  return Math.round((now.getTime() - startOfYear) / (endOfYear - startOfYear) * 1000) / 10
+}
+
+function getBar(percent: number): string {
+  return "▓".repeat(percent / 3) + "░".repeat((100 - percent) / 3)
+}
+
 const argv = yargs
-  .command('hello', 'say hello')
-  .command('hoge', 'fuga')
-  .demandCommand(1)
+  .option('dry-run', {
+    alias: '-d',
+    description: 'Only progress percent',
+    demandCommand: false
+  })
   .help()
   .argv
+const percent = getPercent()
+const message = `⏳ ${new Date().getFullYear()} is ${percent}% complete.
 
-switch (argv._[0]) {
-  case 'hello':
-    console.log('hello ysmt!')
-    break;
-  case 'hoge':
-    console.log('hey hoge!')
-  default:
-    break;
-}
+${getBar(percent)}`
+const readme = `## Hi there! I'm [ysmtegsr](https://ysmtegsr.com/about?to=github) :stuck_out_tongue_winking_eye:
+
+![](https://komarev.com/ghpvc/?username=ysmtegsr&style=flat-square&label=visitors&color=05122A)
+[![progress](https://github.com/ysmtegsr/ysmtegsr/actions/workflows/progress.yml/badge.svg)](https://github.com/ysmtegsr/ysmtegsr/actions/workflows/progress.yml)
+
+---
+
+${message}
+
+inspired by [@ProgressBar202_](https://twitter.com/ProgressBar202_)`
+
+console.log(argv.dryRun ? message : readme)
